38f206e207596d7117c8df238ac33804dfb62503
shahn3 <Niraj@Nirajs-MacBook-Air.local>
2016-10-12 14:51:51 +0530

    Commit 0.4
    Now if I put my circle over another circle then click then it becomes a
    color between 0 and 255. I want to take out the click next.

--- sketch.js
+++ sketch.js
@@ -9,54 +9,53 @@ var circleColor = [];
 
 function setup() 
 {
-  createCanvas(1250,750);
+  createCanvas(1250, 750);
   background(0);
-  for (var y = 0; y < height/150; y++)
+  for (var y = 0; y < height / 150; y++) 
   {
-    for (var x = 0; x < width/150; x++)
+    for (var x = 0; x < width / 150; x++) 
     {
-      circleX.push(x*150+275);
-      circleY.push(y*150+80);
-      circleColor.push(color(random(0,256), random(0,256), random(0,256)));
+      circleX.push(x * 150 + 275);
+      circleY.push(y * 150 + 80);
+      circleColor.push(color(random(0, 256), random(0, 256), random(0, 256)));
     }
   }
-  
+
 }
 
 function draw() 
 {
-  hitCircles();
   startScreen();
-  if (mouseIsPressed)
+  hitCircles();
+  if (mouseIsPressed) 
   {
-  makeCircles();
+    makeCircles();
   }
 }
 
-function startScreen()
+function startScreen() 
 {
   textSize(32);
   textFont("Georgia");
   textAlign(CENTER);
   fill(255);
-  text(text1,300,300,700,500);
+  text(text1, 300, 300, 700, 500);
 }
 
-function makeCircles()
+function makeCircles() 
 {
   background(0);
-  for ( var i = 0; i < circleX.length; i++)
+  for (var i = 0; i < circleX.length; i++) 
   {
     fill(circleColor[i]);
     ellipse(circleX[i], circleY[i], 150, 150);
   }
 }
 
-function hitCircles()
+function hitCircles() 
 {
-  background(0);
   fill(255);
-  ellipse(positionX,positionY,70,70)
+  ellipse(positionX, positionY, 70, 70)
   if (keyIsDown(UP_ARROW)) 
   {
     positionY -= 4;
@@ -75,27 +74,11 @@ function hitCircles()
   {
     positionX += 4;
   }
-  for ( var i = 0; i < circleX.length; i++)
+  for (var i = 0; i < circleX.length; i++) 
   {
-  if (dist(positionX,positionY,circleX[i],circleY[i]) < 110)
-  {
-    circleColor[i] = color(255);
-  }
+    if (dist(positionX, positionY, circleX[i], circleY[i]) < 110) 
+    {
+      circleColor[i] = color(random(0,256));
+    }
   }
 }
-
-//1. Make the array of circles 
-//2. Assign the colors. 
-//3. Color the circles using the fill() function 
-//5. Change the colors to 255 when collides. 
-
-
-
-
-
-
-  
-
-
-
-
